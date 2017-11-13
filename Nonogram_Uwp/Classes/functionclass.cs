15bebeb0e5edeed5703e17f82eef2b7d345b717c
Gijs Zwegers <PS160511@summacollege.nl>
2017-11-13 23:00:54 +0100

    outlined some code

--- Nonogram_Uwp/Classes/functionclass.cs
+++ Nonogram_Uwp/Classes/functionclass.cs
@@ -12,16 +12,16 @@ namespace Nonogram_Uwp.Classes
 {
     class functionclass
     {
-        public static IEnumerable<Color> GetPixels(Image bitmap)
-        {
-            for (int x = 0; x < bitmap.Width; x++)
-            {
-                for (int y = 0; y < bitmap.Height; y++)
-                {
-                    //Color pixel = bitmap.GetPixel(x, y);
-                    yield return pixel;
-                }
-            }
-        }
+        //public static IEnumerable<Color> GetPixels(Image bitmap)
+        //{
+        //    for (int x = 0; x < bitmap.Width; x++)
+        //    {
+        //        for (int y = 0; y < bitmap.Height; y++)
+        //        {
+        //            //Color pixel = bitmap.GetPixel(x, y);
+        //            yield return pixel;
+        //        }
+        //    }
+        //}
     }
 }
